bad32244ab5a2447a11e4ac6df26c507a39147c6
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2023-04-03 16:01:44 +0300

    Выполнено

--- task_2.2.py
+++ task_2.2.py
@@ -20,3 +20,9 @@ def quarter_of(month):
         return 3
     else:
         return 4
+
+#     Да вполне, можно и через словари)
+
+def quarter_of(month):
+    q = {1: (1,3), 2:(4,6), 3:(7,9), 4:(10,12)}
+    return [k for k,v in q.items() if v[0] <= month <= v[1]][0]
